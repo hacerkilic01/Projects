b74daa661b2af678c0baa69215a82a0b6120bb6f
Hacer <hacerkilic194@gmail.com>
2023-10-28 15:47:37 +0300

    link

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,31 @@
+import Link from 'next/Link';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* HTML belgesinin dilini belirtmek için  */}
       <body suppressHydrationWarning={true}>
-        <header style={{ border: "solid blue 1px" }}>[header]</header>
-        <main>{children}</main>
-        <footer style={{ border: "solid blue 1px" }}>[footer]</footer>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {children}
+          </main>
+        <footer>
+       Game <a href="https://rawg.io/" target="_blank">RAWG</a>
+        </footer>
+        
       </body>
     </html>
   );
