ef6802140607794f0e6eed3cef173a4c1927430d
Hacer <hacerkilic194@gmail.com>
2023-10-31 20:02:25 +0300

    link

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,9 +1,17 @@
+import Link from 'next/link';
 export default function ReviewsPage() {
     return (
       <>
         {/* birden fazla JSX öğesini gruplamak için */}
         <h1>Reviews</h1>
-        <p> List is reviews</p>
+      <ul>
+        <li>
+          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+        </li>
+        <li>
+          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        </li>
+      </ul>
       </>
     );
   }
